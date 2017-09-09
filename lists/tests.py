44b98d64a63f1eeaacb856dfcdfbb4ea3bfbe347
Dave Green <greendavegreen@gmail.com>
2017-09-09 14:41:50 -0400

    puts currently submitted item into list, but error appears when second items is added and first is not remembered.

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ from django.urls import resolve
 from django.test import TestCase
 from .views import home_page
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 # Create your tests here.
 
@@ -18,10 +19,25 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+
+    def test_home_page_returns_right_template(self):
+        # This test is preferred to static text, or even reading the html file
+        # bytes and comparing them to the returned bytes because the processor
+        # is going to be altering the template before returning it to the caller
+
+        # all you can really hope for is that the template is used.
+
+        # this renders the first test unecessary since it confirms that the / url is valid
+        # and resolves.
+
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
